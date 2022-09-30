2e7940c505058b87666232e19105634df9637df2
dusklight00 <r.rahul.developer@gmail.com>
2022-09-30 14:04:16 +0530

    configured central api with frontend (#8)

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from engine.engine import get_best_move
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder = "static"    
+)
 
-@app.route('/get_best_move', methods=['GET'])
+@app.route("/", methods = ["GET"])
+def main_game():
+    return render_template("chess-board.html")
+
+@app.route("/get_best_move", methods = ["GET"])
 def get_best_move_endpoint():
-    fen = request.args.get('fen')
+    fen = request.args.get("fen")
     best_move = get_best_move(fen)
-    return jsonify(best_move=best_move)
+    return jsonify(best_move = best_move)
+
+if __name__ == "__main__":
+    app.run(debug = True)
